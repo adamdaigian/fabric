b83472cd3194cd1e7e83e0a49fc87326512951ee
Adam Daigian <adam.daigian@gmail.com>
2013-08-08 14:31:26 -0700

    first commit. adding blank rails app for starting project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabric::Application.config.secret_token = '9e7264ec7af4b930b8627d774a33d65bb64b35d651a706b3f4e50eeaaf2eebccb1cd9356a73d8a75de36f79c210654d40c80c447f6eeeb6abc4feed4762fb2c2'
