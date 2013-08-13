d42c8c88f8938c78dd4e06dafb9864f74f80a246
Neal Barbaro <nealbarbaro@gmail.com>
2013-08-13 10:19:51 -0700

    implement jane's suggestions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,34 +17,18 @@ class StaticPagesController < ApplicationController
         begin
           long_url = LongURL.expand(short_url)
           @tweet_urls << long_url
-        rescue
-        end
-      end
-    end
-
-  end
-end
-
-#     @tweets.each do |tweet|
-#       unless tweet.attrs[:entities][:urls][0].nil?
-#         short_url = tweet.attrs[:entities][:urls][0][:expanded_url]
-#         begin
-#           long_url = LongURL.expand(short_url)
-#           @tweet_urls << long_url
-#           if Article.where(url:long_url).exists?
+#           if Article.where(url:long_url).exists?
 #             article = Article.where(url:long_url).first
 #             current_user.articles << article
 #             return article
 #           else
-#           source = open(long_url).read
-#           content = Readability::Document.new(source, :tags => %w[div p img a strong em], :attributes => %w[src href], :remove_empty_nodes => true).content
-#           current_user.articles << Article.create(url:long_url, content:content)
+#             source = open(long_url).read
+#             content = Readability::Document.new(source, :tags => %w[div p img a strong em], :attributes => %w[src href], :remove_empty_nodes => true).content
+#             current_user.articles << Article.create(url:long_url, content:content)
 #           end
-#         rescue
-#         end
-#       end
-#     end
-    # source = open(@tweet_urls.first).read
-    # @first_article = Readability::Document.new(source, :tags => %w[div p img a strong em], :attributes => %w[src href], :remove_empty_nodes => true).content
-  end
+        rescue
+        end
+      end
+    end
+  end
 end
