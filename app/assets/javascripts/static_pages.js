e4a50ec299d653ee56336407b040c08e07dc4d05
Adam Daigian <adam.daigian@gmail.com>
2013-08-14 11:30:00 -0700

    solved horizontal scrolling issues

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+  $.fn.dontScrollParent = function () {
+    return this.bind("mousewheel DOMMouseScroll", function (e) {
+      var t;
+      return t = e.originalEvent.wheelDelta || -e.originalEvent.detail, t > 0 && $(this).scrollTop() <= 0 ? !1 : t < 0 && $(this).scrollTop() >= this.scrollHeight - $(this).height() ? !1 : !0
+    })
+  };
+
+  // $('.wrapper').dontScrollParent();
+
   var renderForm = function(){
     console.log("called renderForm function")
     selectedSentence = $(this).attr("id")
