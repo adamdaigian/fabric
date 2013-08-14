970d3bb2f31ecc9548ab530e103c119ef6eac69a
Adam Daigian <adam.daigian@gmail.com>
2013-08-13 18:05:43 -0700

    working master to be pushed to git

--- db/migrate/20130813233416_articles_users.rb
+++
@@ -1,7 +0,0 @@
-class ArticlesUsers < ActiveRecord::Migration
-def self.up
-  create_table 'articles_users', :id => false do |t|
-    t.column :article_id, :integer
-    t.column :user_id, :integer
-  end
-end
