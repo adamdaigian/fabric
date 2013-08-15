b36fe1d509cd68bbb0c2de4bee7240ce24f4b00c
Neal Barbaro <nealbarbaro@gmail.com>
2013-08-15 10:44:24 -0700

    getting close

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
   $("#annotate_window").hide();
 
-
-
   // $('.wrapper').dontScrollParent();
 
   var renderForm = function(){
-    console.log("renderForm");
     selectedSentence = $(this).attr("id");
     $.ajax({
       dataType: "script",
@@ -16,11 +13,8 @@ $(document).ready(function(){
   }; // renderForm
 
   var submitForm = function(){
-    console.log("submitForm")
     sentence_id = $('#sentence_id').val();
-    console.log("sentence_id is " + sentence_id);
     annotation_content = $("#annotation_field").val();
-    console.log("annotation_content is " + annotation_content)
     $.ajax({
       dataType: "script",
       type: "get",
@@ -29,6 +23,17 @@ $(document).ready(function(){
   }; // submitForm
 
   // Event Handlers
+
+  var removeModal = function(){
+    $('#annotate_window').hide()
+  };
+
+  $('body').on('click', '.icon-remove', removeModal);
+  // $('.icon-remove').click(function() {
+
+  // });
+
+
   $("body").on("click", ".sentence", renderForm);
   $("body").on("click", "#submit_annotation", submitForm);
 
